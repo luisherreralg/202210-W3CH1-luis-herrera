b4ef9bf08f3b8a3e3b8129c25072910dc5c95711
luisherreralg <lherreralg@outlook.es>
2022-10-17 22:53:24 +0200

    updated a compiled file

--- dist/generateCard.js
+++ dist/generateCard.js
@@ -1,11 +1,4 @@
-import { cardTemplate } from './cardTemplate.js';
-import { characters } from './characters.js';
 export const generateCard = () => {
-    const data = characters;
-    const card1 = cardTemplate(data[0]);
-    const card1 = cardTemplate(data[1]);
-    const eDiv = document.querySelectorAll('.character.col');
-    eDiv.outerHTML = card1;
     //   data.forEach((element) => {
     //     const eDiv = document.querySelectorAll('.character__card');
     //     (eDiv as HTMLDivElement).outerHTML = cardTemplate(element);
